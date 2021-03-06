ca4737372eed8f0508dd9b956412ea011d1fc155
Roshan Varghese <roshan.varghese612@gmail.com>
2021-03-07 01:03:17 +0530

    Update ReverseLL.cpp

--- Linked List/ReverseLL.cpp
+++ Linked List/ReverseLL.cpp
@@ -1,1 +1,18 @@
 //
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(!head) return NULL;
+        
+        ListNode *prev = NULL;
+
+        while(head!=NULL){
+            ListNode *next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+        }
+        
+        return prev;
+    }
+};
