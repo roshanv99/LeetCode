47c7c9a142b26133cb14a9caf6db90b5f07aa5ef
Roshan Varghese <roshan.varghese612@gmail.com>
2021-03-13 22:35:52 +0530

    Create Remove8and53.py

---
+++ Arrays/Remove8and53.py
@@ -0,0 +1,25 @@
+#Remove the numbers 8 and 53 from the sequence and also convert to lower case
+
+n = list(str(input("enter string: ")))
+if not "".join(n).isalnum():
+    print("INVALID")
+    
+count = 0
+i = 0
+
+while(i<len(n)-1):
+    i+=1
+    n[i-count] = n[i].lower()
+    if n[i]=="8":
+        count+=1
+
+    if (i<len(n)-2) and n[i] == "5" and n[i+1] == "3":
+        print('!!! ' + str(n[i]) + str(n[i+1]))
+        i  +=1
+        count+=2
+
+    
+    print(str(i) + ': ' + ''.join(n) + ' : ' + str(count))
+print(''.join(n[:i-count+1]))
+
+
