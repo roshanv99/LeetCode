5b254e2b3feb08099a76d89eebce987439c64a1f
Roshan Varghese <roshan.varghese612@gmail.com>
2021-03-19 19:16:48 +0530

    Create FirstUniqueCharacter.py

---
+++ Strings/FirstUniqueCharacter.py
@@ -0,0 +1,22 @@
+'''
+s = "leetcode"
+return 0.
+
+s = "loveleetcode"
+return 2.
+
+'''
+
+
+
+class Solution:
+    def firstUniqChar(self, s):
+        d = {}
+        seen = set()
+        for idx, c in enumerate(s):
+            if c not in seen:
+                d[c] = idx
+                seen.add(c)
+            elif c in d:
+                del d[c]
+        return min(d.values()) if d else -1
