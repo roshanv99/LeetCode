7bf8c7ec039ab2c4f4ffa63de92ad6d1a84e4532
Roshan Varghese <roshan.varghese612@gmail.com>
2021-05-24 11:49:57 +0530

    Create LL_ReverseLL_Iterative.py

---
+++ Linked List/LL_ReverseLL_Iterative.py
@@ -0,0 +1,12 @@
+class Solution:
+# @param {ListNode} head
+# @return {ListNode}
+
+def reverseList(self, head):
+    prev = None
+    while head:
+        curr = head
+        head = head.next
+        curr.next = prev
+        prev = curr
+    return prev
